22995f869cca91b5b56c5f8e3816a4561368ae94
Patrick <pschilber@gmail.com>
2023-07-18 17:34:54 -0400

    worked magic and kinda got things working

--- index.js
+++ index.js
@@ -8,33 +8,49 @@ function add() {
    } else {
       document.getElementById('todoList').appendChild(li);
    }
-   document.getElementById("textInput").value = "";
+   document.getElementById("textInput");
+   remove()
 }
 
-// const appendCompleted = document.getElementById('todoList');
-// todoList.addEventListener('click', (event) => {
-//    event.target.appendChild("#completedList")
-// });
-
-const todoList = document.getElementById('todoList');
-todoList.addEventListener('click', (event) => {
-   event.target.remove();
-});
-
-
-function move() {
-   let li = document.createElement("li");
-   let textInput = document.getElementById("text").value
-   let todoItem = document.createTextNode(textInput);
-   li.appendChild(todoItem);
-   if (textInput === '') {
-      alert("You must write something!");
-   } else {
-      document.getElementById('todoList').appendChild(li);
+function remove() {
+   const lis = document.querySelectorAll('li');
+   for(let li of lis) {
+   li.addEventListener('click', (event) => {
+      li.remove();
+      move(li.innerText);
+   });
    }
-   document.getElementById("textInput").value = "";
 }
 
+remove();
+
+function move (completedItem) {
+   let li = document.createElement("li");
+   completedItem = document.createTextNode(completedItem);
+   li.appendChild(completedItem);
+   document.getElementById("completedList").appendChild(li);
+   }
+
+// function move(li) {
+//    const todo = li;
+//    const complete = document.getElementById('completedList');
+//    // const element = todo.firstElementChild;
+//    if(li)
+//       complete.appendChild(li);
+// }
+
+// function move() {
+//    let li = document.createElement("li");
+//    let textInput = document.getElementById("text").value
+//    let todoItem = document.createTextNode(textInput);
+//    li.appendChild(todoItem);
+//    if (textInput === '') {
+//       alert("You must write something!");
+//    } else {
+//       document.getElementById('todoList').appendChild(li);
+//    }
+//    document.getElementById("textInput").value;
+// }
 
 function clearInput() {
    document.getElementById("text").reset();
