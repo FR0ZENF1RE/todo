d90bf80b929b66bd3bb31e9499704f1b67a653ba
FR0ZENF1RE <137827333+FR0ZENF1RE@users.noreply.github.com>
2023-07-18 11:22:47 -0800

    completed styling

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-function add () {
-     let li = document.createElement("li");
-     let textInput = document.getElementById("text").value
-     let todoItem = document.createTextNode(textInput);
-     li.appendChild(todoItem);
-     if (textInput === '' ) {
-        alert("You must write something!");
-     }else{
-        document.getElementById('todoList').appendChild(li);
-     }
-     document.getElementById("textInput").value = "";
-     }
+function add() {
+   let li = document.createElement("li");
+   let textInput = document.getElementById("text").value
+   let todoItem = document.createTextNode(textInput);
+   li.appendChild(todoItem);
+   if (textInput === '') {
+      alert("You must write something!");
+   } else {
+      document.getElementById('todoList').appendChild(li);
+   }
+   document.getElementById("textInput").value = "";
+}
 
-     const todoList = document.querySelector('ul');
-  todoList.addEventListener('click', (event) => {
-            event.target.remove(); 
-         });
+// const appendCompleted = document.getElementById('todoList');
+// todoList.addEventListener('click', (event) => {
+//    event.target.appendChild("#completedList")
+// });
+
+const todoList = document.getElementById('todoList');
+todoList.addEventListener('click', (event) => {
+   event.target.remove();
+});
+
+
+function move() {
+   let li = document.createElement("li");
+   let textInput = document.getElementById("text").value
+   let todoItem = document.createTextNode(textInput);
+   li.appendChild(todoItem);
+   if (textInput === '') {
+      alert("You must write something!");
+   } else {
+      document.getElementById('todoList').appendChild(li);
+   }
+   document.getElementById("textInput").value = "";
+}
+
+
+function clearInput() {
+   document.getElementById("text").reset();
+}
+
