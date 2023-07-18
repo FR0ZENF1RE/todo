c39821c7e283eb3b83f75ca8393b2f3e30465b7a
root <root@LAPTOP-14ILROJK>
2023-07-18 12:59:08 -0400

    Added function to remove items

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+function add () {
+     let li = document.createElement("li");
+     let textInput = document.getElementById("text").value
+     let todoItem = document.createTextNode(textInput);
+     li.appendChild(todoItem);
+     if (textInput === '' ) {
+        alert("You must write something!");
+     }else{
+        document.getElementById('todoList').appendChild(li);
+     }
+     document.getElementById("textInput").value = "";
+     }
 
-document.getElementById('addItem').onclick = function() {
-    let newItem = prompt('Please Enter Your New To Do Item!');
-    document.getElementById('todoList').innerText = newItem;
-}
+     const todoList = document.querySelector('ul');
+  todoList.addEventListener('click', (event) => {
+            event.target.remove(); 
+         });
